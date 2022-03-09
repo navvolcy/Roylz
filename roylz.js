bbd964fdd74fac9ab3435d64291aa7e0de34ab3f
Nav <navvolcy@gmail.com>
2022-03-09 10:11:45 -0700

    part one

---
+++ roylz.js
@@ -0,0 +1,49 @@
+const league = [
+    {
+
+        Name: "TwinCross14",
+        Rank: "Silver IV",
+        Roles:"Jungler",
+        Champion:"Kayn"
+
+},
+{
+    Name: "Graciasp",
+    Rank: "Gold IV",
+    Roles:"Mid-Laner",
+    Champion: "Ekko"
+
+},              
+{
+    Name: "Knowlty",
+    Rank: "Bronze I",
+    Roles:"Bot-Laner",
+    Champion:"Tristana"
+
+},
+{
+    Name: "shimgro",
+    Rank: "Silver IV",
+    Roles:"Support",
+    Champion: "Lux"
+
+},
+{   
+    Name:"Franchement",
+    Rank: "Silver III",
+    Roles: "Top-Laner",
+    Champion:"Nasus"
+
+},
+]
+
+league.forEach(league =>{
+    
+    //create instance of the element specified for tag
+    const container = document.createElement("div");
+    const playList = document.createElement("ul");
+    const players = document.createElement("li");
+
+    
+
+})
