2e817faaf8419085a87c12e4d7cc7b77bc8f6dd0
Nav <navvolcy@gmail.com>
2022-03-09 18:18:41 -0700

    updated to javascript

--- roylz.js
+++ roylz.js
@@ -1,27 +1,30 @@
 const league = [
     {
-
+        pic : "./kayn.png",
         Name: "TwinCross14",
         Rank: "Silver IV",
         Roles:"Jungler",
         Champion:"Kayn"
 
 },
-{
+{   
+    pic : "./Ekko.png",
     Name: "Graciasp",
     Rank: "Gold IV",
     Roles:"Mid-Laner",
     Champion: "Ekko"
 
 },              
-{
+{   
+    pic : "./tristana-little-demon.png",
     Name: "Knowlty",
     Rank: "Bronze I",
     Roles:"Bot-Laner",
     Champion:"Tristana"
 
 },
-{
+{   
+    pic: "./lux.png",
     Name: "shimgro",
     Rank: "Silver IV",
     Roles:"Support",
@@ -29,6 +32,7 @@ const league = [
 
 },
 {   
+    pic: "./Nasus.png",
     Name:"Franchement",
     Rank: "Silver III",
     Roles: "Top-Laner",
@@ -36,14 +40,60 @@ const league = [
 
 },
 ]
+console.log("hello")
 
 league.forEach(league =>{
-    
     //create instance of the element specified for tag
     const container = document.createElement("div");
+    const photoPic = document.createElement("img");
     const playList = document.createElement("ul");
-    const players = document.createElement("li");
+    const players1 = document.createElement("li");
+    const players2 = document.createElement("li");
+    const players3 = document.createElement("li");
+    const players4 = document.createElement("li");
+    const players5 = document.createElement("li");
+
+
+    //nesting the element tags 
+    container.appendChild(playList);
+    playList.appendChild(players1);
+    playList.appendChild(players2);
+    playList.appendChild(players3);
+    playList.appendChild(players4);
+    playList.appendChild(players5);
+    players1.appendChild(photoPic);
+
+    // creating the text
+    const newPic = document.createTextNode(league.pic);
+    const newContain = document.createTextNode(league.Name);
+    const  newRank = document.createTextNode(league.Rank);
+    const newRoles = document.createTextNode(league.Roles);
+    const newChamp = document.createTextNode(league.Champion);
+
+    // appending to each list tag
+    players2.appendChild(newContain);
+    players3.appendChild(newRank);
+    players4.appendChild(newRoles);
+    players5.appendChild(newChamp);
+
+
+
+    photoPic.src = league.pic;
+
+
+
+    document.body.appendChild(container);
+
+});
+
+
+
+
+
+   
+
+
+   
 
     
 
-})
